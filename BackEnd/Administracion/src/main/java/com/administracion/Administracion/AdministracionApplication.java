6bc018eac95c9113077816efc257c02f1138b44d
Ezequiel Giannoni <egiannoni@frba.utn.edu.ar>
2022-11-28 14:12:23 -0300

    PrimeraVersion v0.0.0

---
+++ BackEnd/Administracion/src/main/java/com/administracion/Administracion/AdministracionApplication.java
@@ -0,0 +1,13 @@
+package com.administracion.Administracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionApplication.class, args);
+	}
+
+}
