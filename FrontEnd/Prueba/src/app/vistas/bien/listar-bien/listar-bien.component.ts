6fe9a0cf1d9eff656ed7a8aecd09db893565ee08
Ezequiel <egiannoni@frba.utn.edu.ar>
2023-01-10 15:37:53 -0300

    Duplicado Bien v0.0.0

--- FrontEnd/Prueba/src/app/vistas/bien/listar-bien/listar-bien.component.ts
+++ FrontEnd/Prueba/src/app/vistas/bien/listar-bien/listar-bien.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Bien } from 'src/app/models/bien';
+import { Cliente } from 'src/app/models/cliente';
+import { BienService } from 'src/app/service/bien.service';
+import { ClienteService } from 'src/app/service/cliente.service';
 
 @Component({
   selector: 'app-listar-bien',
   templateUrl: './listar-bien.component.html',
   styleUrls: ['./listar-bien.component.css']
 })
-export class ListarBienComponent {
+export class ListarBienComponent implements OnInit {
 
+  bien: Bien[] = [];
+
+  cliente: Cliente[] = [];
+
+  constructor(private bienService: BienService, private service: ClienteService) { }
+
+  ngOnInit() {
+      /*this.bienService.getProductsSmall().then(data => this.products = data);
+      this.clienteService.getCustomersLarge().then(customers => this.customers = customers);*/
+  }
 }
